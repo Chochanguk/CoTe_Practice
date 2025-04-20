b81ac09fa6f15b4d813545dab2fcc73b12aeffa5
조창욱 <119058637+Chochanguk@users.noreply.github.com>
2025-04-20 16:29:36 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 57.58 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -4,30 +4,36 @@ def broute_force(m, n, board):
     # 2x2 블록 확인
     for i in range(m - 1):
         for j in range(n - 1):
-            if board[i][j] != '0' and \
-               board[i][j] == board[i][j+1] == board[i+1][j] == board[i+1][j+1]:
-                removed |= {(i, j), (i, j+1), (i+1, j), (i+1, j+1)}
+            if board[i][j] != '0' and board[i][j] == board[i][j+1] == board[i+1][j] == board[i+1][j+1]:
+                removed |= {(i, j), (i, j+1), (i+1, j), (i+1, j+1)} # set에 집합 추가하기
 
     cnt = len(removed)
 
     # 제거
     for x, y in removed:
         board[x][y] = '0'
-
-    # 아래로 떨어뜨리기
+    new_board=[]
+    
+    # board 아래로 밀기
     for j in range(n):
-        col = [board[i][j] for i in range(m) if board[i][j] != '0']
-        for i in range(m - len(col)):
-            board[i][j] = '0'
-        for i in range(len(col)):
-            board[m - len(col) + i][j] = col[i]
-
-    return cnt, board
+        row=[] # 0이 아닌것들 집합
+        for i in range(m):
+            if board[i][j]!='0':
+                row.append(board[i][j])
+                
+        zeros=['0']*(m-len(row))
+        row=zeros+row
+        for i in range(m):
+            board[i][j]=row[i]
+        
+#         print(row)
+#     print(board)
+        
+    return cnt,board
 
 def solution(m, n, board):
     board = [list(row) for row in board]
     total = 0
-
     while True:
         cnt, board = broute_force(m, n, board)
         if cnt == 0:
