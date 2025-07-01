a633df551d02a725e67f1cc1c3a86f80c3ac8496
조창욱 <119058637+Chochanguk@users.noreply.github.com>
2025-07-01 18:19:54 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 954.90 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -1,17 +1,26 @@
+from copy import deepcopy
+
 def solution(board):
     n = len(board)
     m = len(board[0])
     max_size = 0
 
     # 기존 board를 복사 (깊은 복사)
-    temp = [[board[i][j] for j in range(m)] for i in range(n)]
-
+    temp=deepcopy(board)
+    idx=0
     for i in range(1, n):
         for j in range(1, m):
+            # (i, j)가 정사각형의 오른쪽 아래 꼭짓점이라고 가정
             if board[i][j] == 1:
+                # 현재 표의 이전 좌표에서 가장 작은 값에+1로 갱신
+                # => 현재 정사각형의 한변의 길이로
                 temp[i][j] = min(temp[i-1][j], temp[i][j-1], temp[i-1][j-1]) + 1
                 max_size = max(max_size, temp[i][j])
-
+#             idx+=1
+#             print(f"===={idx} 번째:{i},{j}====")
+#             print(temp)
+#             print()
+            
     # 가장 큰 정사각형이 맨 첫 줄/열에 있을 경우를 위해 예외 처리
     if max_size == 0:
         for i in range(n):
